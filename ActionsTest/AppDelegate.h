33c4501b397e91023dcf2c2060c8725d31e9e7a1
pawelw <talktopawel@gmail.com>
2012-10-19 16:34:56 +0100

    Initial Commit

---
+++ ActionsTest/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  ActionsTest
+//
+//  Created by Pawel Witkowski on 19/10/2012.
+//  Copyright (c) 2012 Pawel Witkowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ ActionsTest/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ActionsTest
+//
+//  Created by Pawel Witkowski on 19/10/2012.
+//  Copyright (c) 2012 Pawel Witkowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
