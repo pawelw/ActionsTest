304a9803a562e1726388379b0e5b2cb2abf97009
pawelw <talktopawel@gmail.com>
2012-11-26 17:17:52 +0100

    General updates

--- ActionsTest/Proxy.m
+++ ActionsTest/Proxy.m
@@ -142,7 +142,7 @@
 
 - (void)request: (XMLRPCRequest *)request didFailWithError: (NSError *)error {
     // Call appDelegation method to pop up "No Internet connection Alert"
-    NSLog(@"Proxy Error: %@", error);
+    NSLog(@"Proxy Error: %@", error.description);
     [delegate didFaultProxyRequest];
 }
 
